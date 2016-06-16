40d5911425308ca079c41aa9220fcbeb638d35ff
kayhide <kayhide@gmail.com>
2016-06-16 22:12:03 +0900

    update 08.

--- questions/08/Main.hs
+++ questions/08/Main.hs
@@ -9,10 +9,18 @@ patterns i route = foldl (++) [] $ map (patterns j) $ map (:route) $ nextPoss ro
         j = i - 1
 
 nextPoss :: Route -> [Pos]
-nextPoss route@((x,y):_) = (not . (`elem` route)) candidates
+nextPoss route@((x,y):_) = filter (not . (`elem` route)) candidates
     where
         candidates = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
 
+count :: Int -> Route -> Int
+count 0 route = 1
+count i route = foldl (+) 0 $ map (count j) $ map (:route) $ nextPoss route
+    where
+        j = i - 1
+
 main :: IO ()
 main = do
-    putStrLn $ show $ length $ patterns 12 [(0, 0)]
+    putStrLn $ show $ count 12 [(0, 0)]
+-- main = do
+--     putStrLn $ show $ length $ patterns 12 [(0, 0)]
