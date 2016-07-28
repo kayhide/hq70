935548c592377fc478ac28742cb035f3fd8261ba
kayhide <kayhide@gmail.com>
2016-07-28 12:22:06 +0900

    update 16 for correct answer.

--- questions/16/Main.hs
+++ questions/16/Main.hs
@@ -1,5 +1,10 @@
+import Control.Monad
+import Data.List
+import Data.Function
+
 type Length = Int
 type Area = Int
+type Tri = (Area, Area, Area)
 
 allLengths :: [Length]
 allLengths = [1..500]
@@ -7,33 +12,35 @@ allLengths = [1..500]
 isSquareValid :: Length -> Bool
 isSquareValid len = len `mod` 4 == 0
 
-exceptSimilar :: [Int] -> [Int]
-exceptSimilar [] = []
-exceptSimilar (x:xs) =
-    x : (filter ((/= 0) . (`mod` x)) (exceptSimilar xs))
-
-allLengthsOfSquareValid :: [Length]
-allLengthsOfSquareValid = filter isSquareValid allLengths
-
 areaVariations :: Length -> [Area]
-areaVariations len = fmap area [1..(half `div` 2 - 1)]
+areaVariations len = do
+    x <- [1..(half `div` 2 - 1)]
+    pure $ x * (half - x)
     where half = len `div` 2
-          area x = x * (half - x)
 
 combinations :: Int -> [a] -> [[a]]
 combinations 0 _ = [[]]
 combinations _ [] = []
 combinations n (x:xs) = (fmap (x:) (combinations (n - 1) xs)) ++ (combinations n xs)
 
-sumVariations :: [Area] -> [Area]
-sumVariations = (fmap sum) . (combinations 2)
-
-isValidLength :: Length -> Bool
-isValidLength len = squareArea `elem` (sumVariations areas)
-    where squareEdge = len `div` 4
-          squareArea = squareEdge * squareEdge
+validTris :: Length -> [Tri]
+validTris len = do
+    [x, y] <- combinations 2 areas
+    guard $ x + y == squareArea
+    pure (squareArea, x, y)
+    where squareArea = (len `div` 4) ^ 2
           areas = areaVariations len
 
+exceptSimilar :: [Tri] -> [Tri]
+exceptSimilar = nubBy ((==) `on` regularize)
+    where regularize (a, x, y) =
+              (fromIntegral x / fromIntegral a, fromIntegral y / fromIntegral a)
+
+run :: [Tri]
+run = do
+    len <- allLengths
+    guard $ isSquareValid len
+    validTris len
+
 main :: IO ()
-main = do
-    print $ filter isValidLength $ exceptSimilar allLengthsOfSquareValid
+main = print $ length $ exceptSimilar run
---
+++ questions/16/README.md
@@ -0,0 +1,5 @@
+```
+% time runghc Main
+20
+runghc Main  1.07s user 0.06s system 99% cpu 1.131 total
+```
