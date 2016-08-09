92fda43bdcd56db3bbb9a6f70d03d9919d3d5a0c
kayhide <kayhide@gmail.com>
2016-08-09 10:56:14 +0900

    update 25 for correct answer.

--- questions/25/Main.hs
+++ questions/25/Main.hs
@@ -13,32 +13,29 @@ combinations 0 _ = [[]]
 combinations _ [] = []
 combinations n (x:xs) = (fmap (x:) (combinations (n - 1) xs)) ++ (combinations n xs)
 
-patterns :: [LinePattern]
-patterns = do
-    ls <- permutations [1..(eyeletsCount - 1)]
-    rs <- permutations [1..(eyeletsCount - 1)]
+patterns :: Int -> [LinePattern]
+patterns n = do
+    ls <- permutations [1..(n - 1)]
+    rs <- permutations [1..(n - 1)]
     return $ toRight (0:ls) (rs ++ [0])
 
 toRight :: [Eyelet] -> [Eyelet] -> LinePattern
 toRight [] _ = []
-toRight (l:ls) rights@(r:rs) = (l, r) : (toLeft ls rights)
+toRight (l:ls) rights@(r:_) = (l, r) : (toLeft ls rights)
 
 toLeft :: [Eyelet] -> [Eyelet] -> LinePattern
 toLeft [] _ = []
-toLeft lefts@(l:ls) (r:rs) = (l, r) : (toRight lefts rs)
+toLeft lefts@(l:_) (r:rs) = (l, r) : (toRight lefts rs)
 
-intersectionCount :: LinePattern -> Int
-intersectionCount lines = sum $ do
-    [x, y] <- combinations 2 lines
-    guard $ ((fst x) - (snd x)) * ((fst y) - (snd y)) < 0
-    return 1
+crossing :: Line -> Line -> Bool
+crossing (l1, r1) (l2, r2) = (l1 - l2) * (r1 - r2) < 0
 
 intersections :: LinePattern -> [(Line, Line)]
 intersections lines = do
     [x, y] <- combinations 2 lines
-    guard $ ((fst x) - (snd x)) * ((fst y) - (snd y)) < 0
+    guard $ crossing x y
     return (x, y)
 
 main :: IO ()
 main = do
-    print $ maximum $ map intersectionCount patterns
+    print $ maximum $ fmap (length . intersections) $ patterns eyeletsCount
---
+++ questions/25/README.md
@@ -0,0 +1,5 @@
+```
+% time runghc Main
+45
+runghc Main  3.61s user 0.07s system 99% cpu 3.705 total
+```
