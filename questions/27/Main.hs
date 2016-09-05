bc1ff63230575ffc47bb8f4125232961465c98b4
kayhide <kayhide@gmail.com>
2016-09-05 23:00:56 +0900

    update 27 for performance.

--- questions/27/Main.hs
+++ questions/27/Main.hs
@@ -1,6 +1,8 @@
+import Data.List
+import Data.Ix
 import Control.Monad
 
-data Direction = East | North | West | South deriving (Eq, Show, Enum)
+data Direction = East | North | West | South deriving (Eq, Ord, Show, Enum)
 
 type Point = (Int, Int)
 type Path = (Point, Direction)
@@ -25,45 +27,36 @@ endPoint :: Path -> Point
 endPoint (pt, dir) = step pt dir
 
 flipPath :: Path -> Path
-flipPath path@(pt, dir) = (endPoint path, turnBack dir)
+flipPath path@(_, dir) = (endPoint path, turnBack dir)
 
 allPaths :: [Path]
 allPaths = do
   x <- [0..width]
   y <- [0..height]
-  dir <- [East, North, West, South]
+  dir <- [East .. South]
   let (x1, y1) = step (x, y) dir
-  guard $ x1 >= 0 && x1 <= width
-  guard $ y1 >= 0 && y1 <= height
+  guard $ inRange (0, width) x1
+  guard $ inRange (0, height) y1
   return ((x, y), dir)
 
+turn :: Int -> Direction -> Direction
+turn angle = toEnum . (`mod` 4) . (+ angle) . fromEnum
+
 turnLeft :: Direction -> Direction
-turnLeft East = North
-turnLeft North = West
-turnLeft West = South
-turnLeft South = East
+turnLeft = turn 1
 
 turnBack :: Direction -> Direction
-turnBack East = West
-turnBack North = South
-turnBack West = East
-turnBack South = North
-
-straightPath :: Path -> Path
-straightPath (p, dir) = (step p dir, dir)
-
-leftPath :: Path -> Path
-leftPath (p, dir) = (step p dir, turnLeft dir)
+turnBack = turn 2
 
 findRoutes :: Route -> [Path] -> [Route]
 findRoutes route@(p:_) paths
   | next == goal = [route]
   | otherwise = do
-      path <- [straightPath p, leftPath p]
+      path <- [(next, dir), (next, turnLeft dir)]
       guard $ path `elem` paths
-      findRoutes (path:route) $ filter (\p -> p /= path && p /= flipPath path) paths
-      -- findRoutes (path:route) $ filter (\p -> p /= path) paths
+      findRoutes (path:route) $ paths \\ [path, flipPath path]
   where next = endPoint p
+        (_, dir) = p
 
 origin :: Path
 origin = ((-1, 0), East)
--- questions/27/README.md
+++ questions/27/README.md
@@ -1,5 +1,5 @@
 ```
 % time runghc Main
 2760
-runghc Main  13.69s user 0.17s system 99% cpu 13.924 total
+runghc Main  2.43s user 0.10s system 99% cpu 2.535 total
 ```
