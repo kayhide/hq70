622b7f04ae4ff2324e4dc8e9ce8a4d599a3972cd
kayhide <kayhide@gmail.com>
2016-10-17 21:12:47 +0900

    create 24.

---
+++ questions/24/Main.hs
@@ -0,0 +1,49 @@
+import Data.List
+
+data Shot = Single Int | Double Int Int deriving (Show)
+
+type Sequence = [Shot]
+
+allShots :: [Shot]
+allShots = (Single <$> [1..9]) ++
+           ((uncurry Double) <$> [(1, 2), (1, 4), (2, 3), (3, 6), (4, 7), (6, 9), (7, 8), (8, 9)])
+-- allShots = (Single <$> [1..3]) ++ [Double 1 2, Double 2 3]
+
+-- |
+-- >>> numbers (Single 2)
+-- [2]
+-- >>> numbers (Double 2 3)
+-- [2,3]
+numbers :: Shot -> [Int]
+numbers (Single x) = [x]
+numbers (Double x y) = [x, y]
+
+-- |
+-- >>> isOverlapping (Single 2) (Double 2 3)
+-- True
+-- >>> isOverlapping (Double 2 3) (Double 3 6)
+-- True
+isOverlapping :: Shot -> Shot -> Bool
+isOverlapping x y = (not . null) $ intersect (numbers x) (numbers y)
+
+-- |
+-- >>> except (Single 2) [Single 1,Single 2,Double 2 3]
+-- [Single 1]
+-- >>> except (Double 1 2) [Single 1,Single 2,Double 2 3]
+-- []
+except :: Shot -> [Shot] -> [Shot]
+except x = filter (not . (isOverlapping x))
+
+-- |
+-- >>> pitch [Single 1,Single 2]
+-- [[Single 1,Single 2],[Single 2,Single 1]]
+-- >>> pitch [Single 1,Single 2,Double 1 2]
+-- [[Single 1,Single 2],[Single 2,Single 1],[Double 1 2]]
+pitch :: [Shot] -> [Sequence]
+pitch [] = [[]]
+pitch shots = do
+  x <- shots
+  fmap (x:) $ pitch (except x shots)
+
+main :: IO ()
+main = print $ length $ pitch allShots
---
+++ questions/24/README.md
@@ -0,0 +1,5 @@
+```haskell
+% time runghc Main
+798000
+runghc Main  12.00s user 0.12s system 99% cpu 12.123 total
+```
