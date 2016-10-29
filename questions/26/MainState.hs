0f276174966ea602e523f2c5ad14c9d5af6741aa
kayhide <kayhide@gmail.com>
2016-10-29 23:12:56 +0900

    update 26 using Control.State.Monad.

---
+++ questions/26/MainState.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+import Data.List
+import Data.Maybe
+import Control.Monad
+import Control.Monad.State
+import Debug.Trace
+
+type Position = (Int, Int)
+type Size = (Int, Int)
+
+data Motion = East | North | West | South deriving (Eq, Show, Enum)
+data Board = Board Position Position deriving (Eq, Show)
+
+type Path = [Motion]
+type PathAndBoard = (Path, Board)
+
+-- size = (3, 2)
+size = (10, 10)
+
+startPosition = (0, 0)
+goalPosition = (width - 1, height - 1)
+  where width = fst size
+        height = snd size
+
+startBoard :: Board
+startBoard = Board startPosition goalPosition
+
+move :: Position -> Motion -> Position
+move (x, y) East = (x + 1, y)
+move (x, y) North = (x, y - 1)
+move (x, y) West = (x - 1, y)
+move (x, y) South = (x, y + 1)
+
+isIn :: Position -> Size -> Bool
+isIn (x, y) (w, h) = 0 <= x && x < w && 0 <= y && y < h
+
+isGoaled :: Board -> Bool
+isGoaled (Board posBlack _) = posBlack == goalPosition
+
+moveVoid :: Board -> Motion -> Maybe Board
+moveVoid (Board posBlack posVoid) mo
+  | posVoid' == posBlack = Just (Board posVoid posVoid')
+  | posVoid' `isIn` size = Just (Board posBlack posVoid')
+  | otherwise = Nothing
+  where posVoid' = move posVoid mo
+
+type Cache = [Board]
+
+nextWithCache :: PathAndBoard -> State Cache [PathAndBoard]
+nextWithCache (path, board) = do
+  cache <- get
+  let next' = catMaybes $ do
+        mo <- enumFrom East
+        return $ do
+          board' <- moveVoid board mo
+          guard $ notElem board' cache
+          return ((mo:path), board')
+  modify (fmap snd next' ++)
+  return next'
+
+queueWithCache :: [PathAndBoard] -> State Cache [PathAndBoard]
+queueWithCache current = do
+  nexts <- sequence $ fmap nextWithCache current
+  let next = concat nexts
+  (next ++) <$> queueWithCache next
+
+main = print $ length $ fst $ evalState find' []
+  where find' = fromJust . find (isGoaled . snd) <$> queueWithCache [([], startBoard)]
--- questions/26/README.md
+++ questions/26/README.md
@@ -9,3 +9,9 @@ runghc Main.hs  21.35s user 0.13s system 100% cpu 21.461 total
 69
 runghc MainSequence.hs  22.50s user 0.16s system 100% cpu 22.632 total
 ```
+
+```haskell
+% time runghc MainState.hs
+69
+runghc MainState.hs  20.93s user 0.14s system 100% cpu 21.038 total
+```
