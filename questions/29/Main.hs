d745e1783602132fe77a46e71bd6b7a6c5518a33
kayhide <kayhide@gmail.com>
2016-11-14 21:36:37 +0900

    update 29 using Rational.

--- questions/29/Main.hs
+++ questions/29/Main.hs
@@ -2,8 +2,9 @@
 
 import Data.List
 import Data.Function
+import Data.Ratio
 
-type Resistance = Double
+type Resistance = Rational
 
 goldenRatio :: Floating a => a
 goldenRatio = (1 + sqrt 5) / 2
@@ -11,10 +12,13 @@ goldenRatio = (1 + sqrt 5) / 2
 resistances :: [Resistance] -> [Resistance]
 resistances [x] = [x]
 resistances (x:xs) =
-  fmap (+x) (resistances xs) ++ fmap (\y -> 1/(1/x + 1/y)) (resistances xs)
+  fmap (+x) (resistances xs) ++ fmap (\y -> recip (recip x + recip y)) (resistances xs)
 
 closest :: Resistance
-closest = minimumBy (compare `on` (abs.(subtract goldenRatio))) $ resistances $ replicate 10 1.0
+closest = minimumBy compare' $ resistances $ replicate 10 1
+  where compare' = compare `on` (abs.(subtract goldenRatio).fromRational)
 
 main :: IO ()
-main = print $ fromIntegral (round (closest * 10^10)) / 10^10
+main = do
+  print closest
+  print $ fromIntegral (round (closest * 10^10)) / 10^10
--- questions/29/README.md
+++ questions/29/README.md
@@ -1,5 +1,6 @@
 ```haskell
 % time runghc Main.hs
+89 % 55
 1.6181818182
-runghc Main.hs  0.35s user 0.26s system 85% cpu 0.711 total
+runghc Main.hs  0.33s user 0.08s system 97% cpu 0.421 total
 ```
